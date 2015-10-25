74d2f3fc269afcf10d97fb371197ed82708857e3
Niels Vroegindeweij <niels@startsmart.nl>
2015-10-25 14:17:40 +0100

    Added nicer overview of available commands

--- ArduinoTestTool/ArduinoTestTool.ino
+++ ArduinoTestTool/ArduinoTestTool.ino
@@ -1,5 +1,11 @@
 #include <SPI.h>
 
+// Message types of Radio connector
+#define KEEPALIVE 1
+#define LISTACCESSPOINTS 2
+#define JOINACCESSPOINT 3
+#define QUITACCESSPOINT 4
+
 const int slaveSelectPin = 10;
 const int interruptPin = 2;
 const int led = 3;
@@ -32,24 +38,53 @@ void loop() {
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
 
-    if(command == "Hello") {
-      char message[] = "Master Alive";
-      
-      digitalWrite(slaveSelectPin,LOW);
-
-      Serial.println("Keep alive sent");
-
-      //SPI.transfer(0x01);
-      //SPI.transfer(sizeof(message));
-      //SPI.transfer(message, sizeof(message));
-
-      SPI.transfer(0x02);
-      SPI.transfer(0x00);
-
-      digitalWrite(slaveSelectPin,HIGH);
-     
-    }
+    // Check for request command
+    if(command == "Alive") {
+      sendKeepAlive();     
+    } else if(command == "ListAp") {
+      sendListAp();
+    } else {
+      Serial.println("Available commands:");
+      Serial.println("Alive  --- Request keep alive from slave");
+      Serial.println("ListAp --- Request accesspoints from slave");
+      Serial.println();
+    }    
   }
+}
+
+/**
+ *  Send Keep Alive to slave 
+ */
+void sendKeepAlive(void) {
+  char message[] = "Master Alive";
+
+  // Sending keep alive
+  Serial.println("---> Sending keep alive");
+
+  // Request keep alive
+  digitalWrite(slaveSelectPin,LOW);
+  
+  SPI.transfer(KEEPALIVE);
+  SPI.transfer(sizeof(message));
+  SPI.transfer(message, sizeof(message));
+  
+  digitalWrite(slaveSelectPin,HIGH);
+}
+
+/**
+ *  Send list accesspoints 
+ */
+void sendListAp() {
+  // Keep alive sent to slave
+  Serial.println("---> Sending list accesspoints");
+
+  // Request accesspoints
+  digitalWrite(slaveSelectPin,LOW);
+  
+  SPI.transfer(LISTACCESSPOINTS);
+  SPI.transfer(0x00);
+  
+  digitalWrite(slaveSelectPin,HIGH);
 }
 
 void incomingMessage() {  
